88b59cfbf11148d0f5bd7cb228586923b700440f
Ivan Vaitusionak <ivan.vaitusionak@rightware.com>
2026-03-20 14:10:10 +0100

    Start checking angles

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from typing import Any
 import cv2
 import numpy as np
 import time
 import requests
+
+CAR_ID: int = 1
+CAR_AUTH: int = 985898
+
+# car_id = 3
+# car_auth = 838748
 
 
 def get_camera_image(id: int) -> bytes:
@@ -19,38 +26,26 @@ def get_camera_image(id: int) -> bytes:
 
     return response.content
 
-
 def rotate(speed: float):
-    #
-    # flip, as a boolean. If true, the wheels will drive in opposite directions, causing you to turn roughly in place.
-    #
+    put(speed, True)
 
-    # car_id = 3
-    # car_auth = 838748
-
-    car_id = 1
-    car_auth = 985898
-
-    # curl -X PUT hackathon-1-car.local:5000  
-    # --header "Content-Type: application/json"   
-    # --header "Authorization: 985898"   
-    # --data '{"speed":0.5,"flip":true}'
-
-    _response = requests.put(
-        f"http://hackathon-{car_id}-car.local:5000",
-        headers={"Authorization": f'{car_auth}'},
-        json={"speed": speed, "flip": True}
-    )
-
-# speed -1, 1
 def move(speed: float):
-    # speed, as a float between -1.0 (drive backwards) and 1.0 (drive forwards). Smaller values cause you to drive more slowly.
-    car_auth = "838748"
-    _response = requests.put(
-        "http://hackathon-3-car.local:5000",
-        headers={"Authorization": car_auth},
-        json={"speed": speed, "flip": False}
+    put(speed, False)
+
+
+def put(speed: float, flip: bool):
+    _ = requests.put(
+        f"http://hackathon-{CAR_ID}-car.local:5000",
+        headers={"Authorization": f'{CAR_AUTH}'},
+        json={"speed": speed, "flip": flip}
     )
+
+
+def angle_between(a, b):
+    dot = np.dot(a, b)
+    cos = np.clip(dot, -1, 1)
+    return np.degrees(np.arccos(cos))
+
 
 # we need to spam these for each second for car to move
 
@@ -98,75 +93,144 @@ def wait_for_key():
         if cv2.waitKey(100) != -1:
             break
 
-# curl hackathon-11-camera.local:50051/frame --header "Authorization: 983149" --output image11.png
-# curl hackathon-12-camera.local:50051/frame --header "Authorization: 378031" --output image12.png
+
+# id -> marker
+def fetch_recent_markers() -> dict[int, Any]:
+    image_png = get_camera_image(11)
+    image = cv2.imdecode(np.frombuffer(image_png, np.uint8), cv2.IMREAD_COLOR)
+
+    dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
+
+    params = cv2.aruco.DetectorParameters()
+    params.errorCorrectionRate = 1.0
+    detector = cv2.aruco.ArucoDetector(dictionary, params)
+
+    corners, ids, _ = detector.detectMarkers(image)
+
+    # id -> corner
+    # 11, 12, 13
+    markers = {int(id): corner for id, corner in zip(ids.flatten(), corners)}
+
+    return markers
+
+
+# marker -> point
+def get_center_point(marker):
+    return np.mean(marker[0], axis=0)
+
+# Hardcoded border markers for camera 11.
+# Note that the last corner is not visible.
+IDS = {
+    13: np.array([149.5,  50.25]),  # top left
+    12: np.array([518.75, 614.25]), # bottom left
+    11: np.array([1157.5, 386.25]), # bottom right
+}
+
+def get_world_perspective_transform(markers):
+    corners = []
+
+    for id, default_center in IDS.items():
+        corner_marker = markers.get(id)
+        if corner_marker is None:
+            corners.append(default_center)
+        else:
+            corners.append(get_center_point(marker=corner_marker))
+
+    corners.append(corners[0] + (corners[2] - corners[1]))
+
+    src = np.array(
+        corners,
+        dtype=np.float32
+    )
+    dst = np.array(
+        [[0, 0], [0, 1], [1, 1], [1, 0]],
+        dtype=np.float32
+    )
+
+    H_mat, _ = cv2.findHomography(src, dst)
+
+    return H_mat
+
+def to_world(ptransform, pixel_point):
+    p = np.array([[pixel_point]], dtype=np.float32)
+    return cv2.perspectiveTransform(p, ptransform)[0][0]
+
+
+
+def rotate_n_times() -> bool:
+    markers = fetch_recent_markers()
+    ptransform = get_world_perspective_transform(markers)
+
+    car_marker = markers.get(CAR_ID)
+    if car_marker is None:
+        print(f"Failed to find a car {CAR_ID}. Retrying..")
+        return False
+
+    car_center_world = to_world(ptransform, get_center_point(car_marker))
+
+    # Wait 2 seconds for car to rotate
+    rotate_power = 0.45
+
+    for _ in range(10):
+        rotate(rotate_power)
+        time.sleep(2)
+
+        markers = fetch_recent_markers()
+
+        car_marker_1 = markers.get(CAR_ID)
+        if car_marker_1 is None:
+            print(f"Failed to find a car {CAR_ID}. Retrying..")
+            continue
+
+        car_center_world_1 = to_world(ptransform, get_center_point(car_marker_1))
+
+        car_rotate_angle = angle_between(car_center_world, car_center_world_1)
+        print(f"{rotate_power} -> {car_rotate_angle}")
+
+        car_center_world = car_center_world_1
+
+
+    return True
 
 def main():
     while True:
-        image_png = get_camera_image(11)
-        with open("temp.png", "wb") as handle:
-            handle.write(image_png)
+        success = rotate_n_times()
+        if success:
+            break
 
-        image = cv2.imdecode(np.frombuffer(image_png, np.uint8), cv2.IMREAD_COLOR)
-
-        dictionary = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_50)
-
-        params = cv2.aruco.DetectorParameters()
-        params.errorCorrectionRate = 1.0
-        detector = cv2.aruco.ArucoDetector(dictionary, params)
-
-
-        corners, ids, rejected = detector.detectMarkers(image)
-
-        # id -> corner
-        # 11, 12, 13
-        markers = {int(id): corner for id, corner in zip(ids.flatten(), corners)}
-        centers = {id: np.mean(corner[0], axis=0) for id, corner in markers.items()}
-
-        for id, center in sorted(centers.items(), key=lambda item: item[1][0]):
-            print(f"{id} -- {center}")
-
-        tl = centers.get(13, np.array([149.5,  50.25]))   # top left
-        bl = centers.get(12, np.array([518.75, 614.25]))  # bottom left
-        br = centers.get(11, np.array([1157.5, 386.25]))  # bottom right
-        tr = tl + (br - bl)
-        print(f"14 -- {tr}")
-
-        src = np.array([tl, bl, br, tr],                 dtype=np.float32)
-        dst = np.array([[0, 0], [0, 1], [1, 1], [1, 0]], dtype=np.float32)
-
-        H_mat, _ = cv2.findHomography(src, dst)
-        def to_world(pixel_point):
-            p = np.array([[pixel_point]], dtype=np.float32)
-            return cv2.perspectiveTransform(p, H_mat)[0][0]
-
-
-
-        canvas = build_world_canvas()
-
-        cv2.aruco.drawDetectedMarkers(image, corners, ids)
-
-        for id, corners in markers.items():
-            if id == 11 or id == 12 or id == 13:
-                continue
-
-            car_corners_world = np.array([to_world(pt) for pt in corners[0]])
-            draw_car_on_canvas(canvas, id, car_corners_world)
-
-
-
-        cv2.imshow("world", canvas)
-        cv2.imshow("out", image)
-
-        wait_for_key()
-
-        cv2.destroyAllWindows()
-
-
-
-        break
 
 if __name__ == "__main__":
-    rotate(0.45)
-    # main()
+    main()
 
+
+
+# 1. Fetch Image
+# 2. Build world transform
+# 3. Find angle to 1.1
+# 4. Rotate
+# 5. Find angle to 1.1
+# 6. compare
+
+
+
+# canvas = build_world_canvas()
+
+
+# for id, corners in markers.items():
+#     if id == 11 or id == 12 or id == 13:
+#         continue
+
+#     car_corners_world = np.array([to_world(pt) for pt in corners[0]])
+#     draw_car_on_canvas(canvas, id, car_corners_world)
+
+# cv2.imshow(f"world{i}", canvas)
+
+# # cv2.aruco.drawDetectedMarkers(image, corners, ids)
+# # cv2.imshow(f"out{i}", image)
+
+# if i == 0:
+#     time.sleep(2)
+# else:
+#     wait_for_key()
+#     cv2.destroyAllWindows()
+#     break
